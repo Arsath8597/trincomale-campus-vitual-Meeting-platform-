eb32c31f7f7893f0772bf85e7d2e1a1faf532ad6
Arsath8597 <arsath8597@gmail.com>
2024-09-17 19:18:49 +0530

    testing 13

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,7 @@ const app =express();
 app.use(cookieParser());
 app.use(express.json())
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://trincomale-campus-vitual-meeting-platform.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
 }));
