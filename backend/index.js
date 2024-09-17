c11bde0407b9d161832db13e8414942495002e6f
Arsath8597 <arsath8597@gmail.com>
2024-09-17 17:49:20 +0530

    testing 10

--- backend/index.js
+++ backend/index.js
@@ -2,13 +2,9 @@ import express from "express";
 import userRoute from "./routes/userRoute.js"
 import cookieParser from "cookie-parser";
 import scheduleRouter from "./routes/scheduleRoute.js"
-import cors from "cors"
+
 const app =express();
-app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  credentials: true,
-}));
+
 
 app.use(cookieParser());
 app.use(express.json())
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import connectDatabse from './config/database.js';
 import app from './index.js';
 import dotenv from 'dotenv';
-
-
+import cors from "cors"
+app.use(cors({
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  }));
 dotenv.config({path:"config/config.env"})
 connectDatabse()
 app.listen(process.env.PORT, () => {
